35d186beadfc13f71587cb9070e924a5bc594741
LiHua <Liva_E@outlook.com>
2017-10-02 10:45:57 +0800

    Android Studio 自动生成原始项目

---
+++ app/src/main/java/cn/edu/stu/max/cocovendor/HomePageActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.stu.max.cocovendor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
