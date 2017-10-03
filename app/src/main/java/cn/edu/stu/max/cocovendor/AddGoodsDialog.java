463c91a77d8dba871bbb63550fef60c6ca32c9b2
LiHua <Liva_E@outlook.com>
2017-10-03 23:26:02 +0800

    增加添加货物的Dialog

---
+++ app/src/main/java/cn/edu/stu/max/cocovendor/AddGoodsDialog.java
@@ -0,0 +1,35 @@
+package cn.edu.stu.max.cocovendor;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.StyleRes;
+import android.view.View;
+
+public class AddGoodsDialog extends Dialog{
+    public Context context;
+
+    public AddGoodsDialog(@NonNull Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    public AddGoodsDialog(@NonNull Context context, @StyleRes int themeResId) {
+        super(context, themeResId);
+        this.context = context;
+    }
+
+    protected AddGoodsDialog(@NonNull Context context, boolean cancelable, @Nullable OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+        this.context = context;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = View.inflate(context, R.layout.add_goods_dialog_layout, null);
+        setContentView(view);
+    }
+}
