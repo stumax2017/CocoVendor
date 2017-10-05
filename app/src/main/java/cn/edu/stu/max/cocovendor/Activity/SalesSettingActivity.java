707537e43b204428833b1a26286e3eec7a7a1ef7
LiHua <Liva_E@outlook.com>
2017-10-05 13:21:12 +0800

    实现添加商品功能

--- app/src/main/java/cn/edu/stu/max/cocovendor/Activity/SalesSettingActivity.java
+++ app/src/main/java/cn/edu/stu/max/cocovendor/Activity/SalesSettingActivity.java
@@ -1,17 +1,26 @@
 package cn.edu.stu.max.cocovendor.Activity;
 
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.widget.SlidingPaneLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.litepal.crud.DataSupport;
+import org.litepal.crud.callback.FindMultiCallback;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import cn.edu.stu.max.cocovendor.JavaClass.AddGoodsDialog;
 import cn.edu.stu.max.cocovendor.R;
@@ -46,9 +55,50 @@ public class SalesSettingActivity extends AppCompatActivity {
         buttonSalesSettingAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AddGoodsDialog addGoodsDialog = new AddGoodsDialog(SalesSettingActivity.this);
-                addGoodsDialog.setTitle(R.string.label_add_goods);
-                addGoodsDialog.show();
+                final ArrayList goodsSelectedItems = new ArrayList();
+                AlertDialog.Builder builder = new AlertDialog.Builder(SalesSettingActivity.this);
+                builder.setTitle(R.string.label_add_goods)
+                        .setMultiChoiceItems(R.array.goods_array, null, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                                if (isChecked) {
+                                    // If the user checked the item, add it to the selected items
+                                    goodsSelectedItems.add(which);
+                                } else if (goodsSelectedItems.contains(which)) {
+                                    // Else, if the item is already in the array, remove it
+                                    goodsSelectedItems.remove(Integer.valueOf(which));
+                                }
+                            }
+                        })
+                        .setPositiveButton(R.string.label_save, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                // FIRE ZE MISSILES!
+                                for (int i = 0; i < goodsSelectedItems.size(); i ++) {
+                                    Goods goods = new Goods();
+                                    goods.setName(getResources().getStringArray(R.array.goods_array)[i]);
+                                    goods.save();
+                                }
+                                DataSupport.findAllAsync(Goods.class).listen(new FindMultiCallback() {
+                                    @Override
+                                    public <T> void onFinish(List<T> t) {
+                                        List<Goods> allGoods = (List<Goods>) t;
+                                        salesSettingAdapter = new SalesSettingAdapter(allGoods);
+                                        //recyclerView显示适配器内容
+                                        recyclerViewSalesSetting.setAdapter(salesSettingAdapter);
+                                    }
+                                });
+                            }
+                        })
+                        .setNegativeButton(R.string.label_cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                            }
+                        });
+                // Create the AlertDialog object and return it
+                builder.setCancelable(false);
+                builder.create().show();
             }
         });
     }
--- app/src/main/java/cn/edu/stu/max/cocovendor/JavaClass/AddGoodsDialog.java
+++ app/src/main/java/cn/edu/stu/max/cocovendor/JavaClass/AddGoodsDialog.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StyleRes;
 import android.view.View;
+import android.widget.Button;
 
 import cn.edu.stu.max.cocovendor.R;
 
@@ -33,5 +34,11 @@ public class AddGoodsDialog extends Dialog{
         super.onCreate(savedInstanceState);
         View view = View.inflate(context, R.layout.add_goods_dialog, null);
         setContentView(view);
+        view.findViewById(R.id.btn_cancel).setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
     }
 }
