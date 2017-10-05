95fd17dcbb8ea8706edd5e12e19ba2b8f6916062
LiHua <Liva_E@outlook.com>
2017-10-05 16:36:17 +0800

    添加系统设置布局文件

---
+++ app/src/main/java/cn/edu/stu/max/cocovendor/Activity/SystemSettingActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.stu.max.cocovendor.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.stu.max.cocovendor.R;
+
+public class SystemSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_setting);
+    }
+}
