5284124dab9f3f176085eeb0316a24135cc1644f
Tony <tony@gmail.com>
2017-10-23 23:49:14 +0800

    修改监控设置

--- app/src/main/java/cn/edu/stu/max/cocovendor/Activity/SystemSettingActivity.java
+++ app/src/main/java/cn/edu/stu/max/cocovendor/Activity/SystemSettingActivity.java
@@ -70,10 +70,8 @@ public class SystemSettingActivity extends AppCompatActivity {
                 case R.id.btn_sales_analyze:
                     break;
                 case R.id.btn_camera_spy:
-                    boolean data = share.getBoolean("isOpen", false);
                     //String data = "true";
                     Intent intent_camera_setting = new Intent(SystemSettingActivity.this, CameraSettingActivity.class);
-                    intent_camera_setting.putExtra("isOpen", data);
                     startActivity(intent_camera_setting);
                     break;
                 case R.id.btn_sys_setting_back:
